4af1420c8b8634deff3f1f903ca4914ecee2387c
Eyder Suarez <ey_der19@hotmail.com>
2024-10-17 16:22:46 -0500

    Inicio del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PeliculasComponent } from './pages/peliculas/peliculas.component';
+import { SeriesComponent } from './pages/series/series.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/peliculas' },
+  { path: 'peliculas', component: PeliculasComponent },
+  { path: 'series', component: SeriesComponent},
+  { path: '**', redirectTo: '/peliculas' },
+];
---
+++ src/app/pages/peliculas/peliculas.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { PeliculasComponent } from './peliculas.component';
+
+describe('PeliculasComponent', () => {
+  let component: PeliculasComponent;
+  let fixture: ComponentFixture<PeliculasComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PeliculasComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PeliculasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
