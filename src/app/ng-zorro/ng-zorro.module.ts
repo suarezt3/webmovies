78b01af5edea31d457cdee5469ec1a3814c8030b
Eyder Suarez <ey_der19@hotmail.com>
2024-10-19 16:38:32 -0500

    Ajuste de la tarjeta

--- src/app/ng-zorro/ng-zorro.module.ts
+++ src/app/ng-zorro/ng-zorro.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    NzButtonModule
+    NzButtonModule,
+    NzIconModule
   ],
   exports: [
-    NzButtonModule
+    NzButtonModule,
+    NzIconModule
   ],
 })
 export class NgZorroModule { }
